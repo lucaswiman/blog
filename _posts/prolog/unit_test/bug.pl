bf62f994c81784c820125e9f0bf0ac51b8868340
Lucas Wiman <lucaswiman@counsyl.com>
2015-12-05 14:24:39 -0800

    Updates to prolog post

--- _posts/prolog/unit_test/bug.pl
+++ _posts/prolog/unit_test/bug.pl
@@ -1,2 +1,15 @@
 add(2, 2, 5) :- !.
-add(X, Y, Z) :- Z is Y + X.
+add(X, Y, Z) :- not((X is 2, Y is 2)) -> Z is Y + X.
+
+
+:- begin_tests(bug).
+test(add) :-
+  add(2, 2, 4)
+.
+
+test(add) :-
+  findall(X, add(2, 2, X), Xs),
+  Xs == [4]
+.
+
+:- end_tests(bug).
